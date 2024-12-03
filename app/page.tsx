ed74a1ee128fb4ca1f87ba6aa145d92e9c4ff779
Mauricio Brito <maubot01@gmail.com>
2024-12-03 10:51:12 -0300

    chore: update font configuration and enhance global styles with new fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] text-sm items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>StackOverflow</h1>
+      <h1 className="h1-bold">StackOverflow</h1>
+      <p className="font-space-grotesk">Hello world</p>
     </div>
   );
 }
