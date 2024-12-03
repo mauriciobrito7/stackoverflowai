6a1e3cb910880dc0d9ae24802c0adae5b9eed8c4
Mauricio Brito <maubot01@gmail.com>
2024-12-03 09:46:33 -0300

    chore: update ESLint configuration and add VSCode settings for formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-	return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-			<h1>StackOverflow</h1>
-		</div>
-	);
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] text-sm items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>StackOverflow</h1>
+    </div>
+  );
 }
