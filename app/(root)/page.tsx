3068a9b17b018f71287d7783d23ac1a1b539da41
Mauricio Brito <maubot01@gmail.com>
2025-01-10 17:40:06 -0300

    feat: refactor layout to include LeftSidebar and adjust main content

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,7 @@
-import { signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
-
-export default async function Home() {
-  const handleLogout = async () => {
-    "use server";
-    await signOut({ redirectTo: ROUTES.SIGN_IN });
-  };
-
+export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] text-sm items-center justify-items-center min-h-scrdiveen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <>
       <h1 className="h1-bold">StackOverflow</h1>
-      <p className="font-space-grotesk">Hello world</p>
-
-      <form action={handleLogout}>
-        <Button type="submit">Logout</Button>
-      </form>
-    </div>
+    </>
   );
 }
