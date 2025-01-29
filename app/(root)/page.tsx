dceb69d92c0ab46b8c59eb54594b2e0954563638
Mauricio Brito <maubot01@gmail.com>
2025-01-29 11:53:40 -0300

    feat: add HomeFilter component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import HomeFilter from "@/components/filters/HomeFilter";
 
 const questions = [
   {
@@ -66,7 +67,9 @@ const Home = async ({ searchParams }: SearchParams) => {
           otherClasses="flex-1"
         />
       </section>
-      {/* HomeFilter */}
+
+      <HomeFilter />
+
       <div className="mt-10 flex w-full flex-col gap-6">
         {filteredQuestions.map((question) => (
           <h1 key={question._id}>{question.title}</h1>
